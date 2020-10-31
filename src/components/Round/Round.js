d927c5a22f99be23d68bc2814340e8d4e8846035
avyrie <pittman.ally@gmail.com>
2020-10-30 22:17:01 -0600

    if true, on click the button turns green, if false, red

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -28,55 +28,47 @@ class Round extends Component {
         
         // Create an answers array with both wrong and right answers ----
         let answers = [];
-
         function answersArr(wrong, right) {
             for(let i = 0; i < wrong.length; i++) {
                 answers.push(JSON.stringify(wrong[i]))
             }
             answers.push(right)
-            console.log(`Answers array ${answers}`)
+            // console.log(`Answers array ${answers}`)
         }
-            
         answersArr(currentIncorrects, currentCorrect);
-        
+        console.log(`Answers array ${answers}`)
+
+        // Randomizes answers array
+        function shuffle(array) {
+            let currentIndex = array.length, tempVal, randomIndex;
+            while (0 != currentIndex) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                tempVal = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = tempVal;
+            }
+            return array;
+        };
+        let shuffledAnswers = shuffle(answers)
+        console.log(shuffledAnswers);
 
 
-        // If you click a choice and it is wrong, function will add false to an array and vice versa ----
+        // If you click a choice and it is wrong, function will add false to an array and vice versa 
         let tally = [];
         function onAnswer(e) {
-            if (e.target.innerHTML == answers[3]) {
-                tally.push(true)
+            if (e.target.innerHTML == currentCorrect) {
+                tally.push(true);
+                e.target.className += "true"
             } else {
-                tally.push(false)
+                tally.push(false);
+                e.target.className += "false"
             }
-            // console.log(`Tally: ${tally}`)
-            // console.log(e.target)
+            console.log(`Tally: ${tally.length}`)
+            return tally;
         }
-
-       
         
-
-
-        // for(let i = 0; i < data.length; i++) {
-        //     incorrectAnswers[i].forEach((j) => {
-        //         answers.push(j)
-        //     })
-        //     // console.log(answers)
-        //     answers.push(correctAnswers[i])
-        //     console.log(answers)
-        // }
-
-        // return parsedQuestion;
-        // console.log(parsedQuestion)
-
-        // console.log(`Props in Round: ${parsedData}`)
-        // Object.entries(data).map(([question, incorrect, correct]) => {
-        //     const parsedQuestion = JSON.stringify(question)
-        //     console.log(`Question: ${parsedQuestion}`)
-        //     console.log(`Incorrect: ${incorrect}`)
-        //     console.log(`Correct: ${correct}`)
-        // })
-
         
 
         return (
@@ -85,10 +77,10 @@ class Round extends Component {
                     <h3>Question: </h3>
                     <p>{currentQuestion}</p>
                     <ul className="round-choices">
-                        <li><button id="choices1" onClick={onAnswer}>{answers[0]}</button></li>
-                        <li><button className="choices" onClick={onAnswer}>{answers[1]}</button></li>
-                        <li><button className="choices" onClick={onAnswer}>{answers[2]}</button></li>
-                        <li><button className="choices" onClick={onAnswer}>{answers[3]}</button></li>
+                        <li><button onClick={onAnswer}>{answers[0]}</button></li>
+                        <li><button onClick={onAnswer}>{answers[1]}</button></li>
+                        <li><button onClick={onAnswer}>{answers[2]}</button></li>
+                        <li><button onClick={onAnswer}>{answers[3]}</button></li>
                     </ul>
                     <button className="next-btn" onClick={this.nextQuestion}>Next</button>
                     {/* <button className="test" onClick={fillArray}>Fill</button> */}
